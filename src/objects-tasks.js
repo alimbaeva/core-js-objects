2505139ade36b6782af437bb58460f905e4645b1
Asel <asel.alimbaeva.kk@gmail.com>
2024-02-27 12:48:02 +0300

    feat: resolved removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -59,8 +59,13 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const result = {};
+  Object.assign(result, obj);
+  keys.forEach((key) => {
+    if (result[key]) delete result[key];
+  });
+  return result;
 }
 
 /**
