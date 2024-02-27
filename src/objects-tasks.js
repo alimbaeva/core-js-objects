f1f49dfb1b6df4b76225064f706bd7cd55e4d711
Asel <asel.alimbaeva.kk@gmail.com>
2024-02-27 15:33:49 +0300

    feat: resolved fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -223,8 +223,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const parsedObject = JSON.parse(json);
+  const objectPrototype = Object.create(proto);
+
+  return Object.assign(objectPrototype, parsedObject);
 }
 
 /**
